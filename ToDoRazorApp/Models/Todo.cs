9565f3d6ad27691db79abebf8e82c03273947490
webmasterdevlin <webmasterdevlin@gmail.com>
2018-04-22 16:36:12 +0800

    Added About, Contact, and NewTask pages.

--- ToDoRazorApp/Models/Todo.cs
+++ ToDoRazorApp/Models/Todo.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace ToDoRazorApp.Models
 {
     public class Todo
     {
-
+        [JsonProperty("_id")]
+        public string Id { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
+        [JsonProperty("isDone")]
+        public bool IsDone { get; set; }
+        [JsonProperty("deadLine")]
+        public DateTime DeadLine { get; set; }
     }
 }
--- ToDoRazorApp/Pages/Index.cshtml.cs
+++ ToDoRazorApp/Pages/Index.cshtml.cs
@@ -1,17 +1,31 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using ToDoRazorApp.Models;
 
 namespace ToDoRazorApp.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private const string Baseurl = "http://localhost:3002/api/todos";
+        public List<Todo> Todos { get; set; }
 
+        public async Task OnGetAsync()
+        {
+            Todos = await GetTodos();
+        }
+
+        public async Task<List<Todo>> GetTodos()
+        {
+            var client = new HttpClient();
+
+            var content = await client.GetStringAsync(Baseurl);
+            var todos = JsonConvert.DeserializeObject<List<Todo>>(content);
+
+            return todos;
         }
     }
 }
